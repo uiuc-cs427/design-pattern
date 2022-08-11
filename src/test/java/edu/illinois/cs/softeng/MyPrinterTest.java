007f569b343c65f6ddb7cd7d19a3f1a9c19b0b29
Lingming Zhang <zhanglm10@gmail.com>
2022-08-11 17:02:40 -0500

    fixing minor issues

--- src/test/java/edu/illinois/cs/softeng/MyPrinterTest.java
+++ src/test/java/edu/illinois/cs/softeng/MyPrinterTest.java
@@ -19,59 +19,65 @@ public class MyPrinterTest
 {
 	/**
 	 * Rigorous Tests :-)
-	 * @throws IOException 
-	 * @throws URISyntaxException 
+	 * 
+	 * @throws IOException
+	 * @throws URISyntaxException
 	 */
 	@Test
 	public void test1() throws IOException, URISyntaxException {
 		String html = "<html><body><ul><li>xx</li></ul></body></html>";
 		Document doc = Jsoup.parse(html); // get initial HTML document
-		MyPrinter printer = new MyPrinter(); 
+		MyPrinter printer = new MyPrinter();
 		String output = printer.prettyPrint(doc);
 		assertEquals(
 				"The output file differs from the expected one: src/test/resources/htmltests/output1.html!",
 				FileUtils.readFileToString(
-						getFileFromResource("htmltests/output1.html"), "utf-8"), output);
+						getFileFromResource("htmltests/output1.html"), "utf-8"),
+				output);
 	}
-	
+
 	@Test
 	public void test2() throws IOException, URISyntaxException {
 		String html = "<html><body><!--p>hello</p--><ul><li></li></ul></body></html>";
 		Document doc = Jsoup.parse(html); // get initial HTML document
-		MyPrinter printer = new MyPrinter(); 
+		MyPrinter printer = new MyPrinter();
 		String output = printer.prettyPrint(doc);
 		assertEquals(
 				"The output file differs from the expected one: src/test/resources/htmltests/output2.html!",
 				FileUtils.readFileToString(
-						getFileFromResource("htmltests/output2.html"), "utf-8"), output);
+						getFileFromResource("htmltests/output2.html"), "utf-8"),
+				output);
 	}
-	
+
 	@Test
 	public void test3() throws IOException, URISyntaxException {
 		String html = "<html><body><!--p>hello</p--><ul><li><a href=\"http://www.google.com\">Google</a></li></ul><p>xx</p></body></html>";
 		Document doc = Jsoup.parse(html); // get initial HTML document
-		MyPrinter printer = new MyPrinter(); 
+		MyPrinter printer = new MyPrinter();
 		String output = printer.prettyPrint(doc);
 		assertEquals(
 				"The output file differs from the expected one: src/test/resources/htmltests/output3.html!",
 				FileUtils.readFileToString(
-						getFileFromResource("htmltests/output3.html"), "utf-8"), output);
+						getFileFromResource("htmltests/output3.html"), "utf-8"),
+				output);
 	}
-	
+
 	/**
 	 * Get the expected output files from the resource directory
+	 * 
 	 * @param fileName
 	 * @return
 	 * @throws URISyntaxException
 	 */
-	private File getFileFromResource(String fileName) throws URISyntaxException{
-        ClassLoader classLoader = getClass().getClassLoader();
-        URL resource = classLoader.getResource(fileName);
-        if (resource == null) {
-            throw new IllegalArgumentException("file not found! " + fileName);
-        } else {
-            return new File(resource.toURI());
-        }
+	private File getFileFromResource(String fileName)
+			throws URISyntaxException {
+		ClassLoader classLoader = getClass().getClassLoader();
+		URL resource = classLoader.getResource(fileName);
+		if (resource == null) {
+			throw new IllegalArgumentException("file not found! " + fileName);
+		} else {
+			return new File(resource.toURI());
+		}
 
-    }
+	}
 }
